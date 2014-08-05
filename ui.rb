086fd08b96a7eb48ea874ddb7d212190c76c6c9f
Katharine Chen & Sanjeev Sekar <student@epicodus.com>
2014-08-05 12:23:18 -0700

    Initial Commit

---
+++ ui.rb
@@ -0,0 +1,41 @@
+require './lib/list'
+require './lib/task'
+require 'pry'
+require 'pg'
+
+DB = PG.connect({:dbname => 'to_do_extra'})
+
+def intro
+  list_menu
+end
+
+def list_menu
+  puts "Please add a list"
+  list_name = gets.chomp
+  puts "Press 't' to add a new task to the list or 'l' to add a new list"
+  answer = gets.chomp.downcase
+  new_list = List.new({'name'=>list_name})
+  # binding.pry
+  new_list.save
+
+  if answer == "t"
+    task_menu(new_list.id)
+  elsif answer == "l"
+    list_menu
+  else
+    puts "Please enter a valid option."
+  end
+end
+
+def task_menu(id)
+
+  puts "Please enter a task"
+  task_description = gets.chomp
+  new_task = Task.new({'name' => task_description, 'list_id' => id})
+  new_task.save
+
+  puts "You have sucessfully saved your task!"
+
+end
+
+intro
